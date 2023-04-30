194fab66a1de0c4f22942b3e3ca64352f37299fa
dasbd72 <twbd723@gmail.com>
2023-05-01 01:44:21 +0800

    update

--- cmd/image-scale/image-scale.go
+++ cmd/image-scale/image-scale.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
+	"image-processing-benchmarks/internal/minioimages"
 	"image/jpeg"
 	"io/fs"
 	"log"
@@ -60,13 +60,12 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Fatalln(err)
 		} else {
-			// minioClient is now set up
 			log.Printf("Connected to %s\n", endpoint)
 		}
 		connect_duration := time.Since(connect_start)
 
 		download_start := time.Now()
-		downloadImages(minioClient, bucketName, downloadPath, localPath)
+		minioimages.DownloadImages(minioClient, bucketName, downloadPath, localPath)
 		download_duration := time.Since(download_start)
 
 		scale_start := time.Now()
@@ -74,7 +73,7 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 		scale_duration := time.Since(scale_start)
 
 		upload_start := time.Now()
-		uploadImages(minioClient, bucketName, localPath, uploadPath)
+		minioimages.UploadImages(minioClient, bucketName, localPath, uploadPath)
 		upload_duration := time.Since(upload_start)
 
 		log.Println("Connect:", connect_duration)
@@ -130,79 +129,4 @@ func scaleImages(localPath string) {
 		return nil
 	})
 	log.Printf("Scaled %d images\n", cnt)
-}
-
-func createBucket(minioClient *minio.Client, bucketName string) {
-	err := minioClient.MakeBucket(context.Background(), bucketName, minio.MakeBucketOptions{})
-	if err != nil {
-		// Check to see if we already own this bucket (which happens if you run this twice)
-		exists, errBucketExists := minioClient.BucketExists(context.Background(), bucketName)
-		if errBucketExists == nil && exists {
-			log.Printf("Bucket %s exists\n", bucketName)
-		} else {
-			log.Fatalln(err)
-		}
-	} else {
-		log.Printf("Bucket %s created\n", bucketName)
-	}
-}
-
-func downloadImages(minioClient *minio.Client, bucketName string, remotePath string, localPath string) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	objectCh := minioClient.ListObjects(ctx, bucketName, minio.ListObjectsOptions{Prefix: remotePath, Recursive: false})
-	for object := range objectCh {
-		if object.Err != nil {
-			fmt.Println(object.Err)
-		}
-		_, filename := filepath.Split(object.Key)
-		minioClient.FGetObject(context.Background(), bucketName, object.Key, localPath+filename, minio.GetObjectOptions{})
-	}
-	log.Printf("Downloaded from %s/%s to %s\n", bucketName, remotePath, localPath)
-}
-
-func uploadImages(minioClient *minio.Client, bucketName string, localPath string, remotePath string) {
-	filepath.Walk(localPath, func(path string, info fs.FileInfo, err error) error {
-		if !info.IsDir() {
-			_, mc_err := minioClient.FPutObject(context.Background(), bucketName, remotePath+info.Name(), path, minio.PutObjectOptions{ContentType: "image/jpeg"})
-			if mc_err != nil {
-				log.Println(mc_err)
-			}
-		}
-		return err
-	})
-	log.Printf("Uploaded from %s to %s/%s\n", localPath, bucketName, remotePath)
-}
-
-func removeImages(minioClient *minio.Client, bucketName string, remotePath string) {
-	// Remove one by one
-	objectsCh := make(chan minio.ObjectInfo)
-	// Send object names that are needed to be removed to objectsCh
-	go func() {
-		defer close(objectsCh)
-		// List all objects from a bucket-name with a matching prefix.
-		for obj := range minioClient.ListObjects(context.Background(), bucketName, minio.ListObjectsOptions{Prefix: remotePath, Recursive: false}) {
-			if obj.Err != nil {
-				log.Fatalln(obj.Err)
-			}
-			objectsCh <- obj
-		}
-	}()
-	for err := range minioClient.RemoveObjects(context.Background(), bucketName, objectsCh, minio.RemoveObjectsOptions{
-		GovernanceBypass: true,
-	}) {
-		fmt.Println("Error detected during deletion: ", err)
-	}
-
-	log.Printf("Removed %s/%s\n", bucketName, remotePath)
-}
-
-func forceRemove(minioClient *minio.Client, bucketName string, remotePath string) {
-	// Remove whole object
-	err := minioClient.RemoveObject(context.Background(), bucketName, remotePath, minio.RemoveObjectOptions{GovernanceBypass: true, ForceDelete: true})
-	if err != nil {
-		log.Println(err)
-	}
-	log.Printf("Removed %s/%s\n", bucketName, remotePath)
 }
