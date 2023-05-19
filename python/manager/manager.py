c41982f82fd1e9596152cb2b385a66d6e9f4a3ee
dasbd72 <twbd723@gmail.com>
2023-05-19 22:19:18 +0800

    autopep8

--- python/manager/manager.py
+++ python/manager/manager.py
@@ -5,6 +5,7 @@ from kubernetes import client, config
 from flask import Flask, request, make_response
 app = Flask(__name__)
 
+
 def get_host_volume_usage(directory):
     # Get disk usage statistics of the specified directory on the host
     usage = shutil.disk_usage(directory)
@@ -28,34 +29,36 @@ def get_mem_usage(v1, nodeName):
     memory_allocatable = None
 
     for node in nodes:
-        #node_name = node.metadata.name
-        #if(node_name==nodeName):
+        # node_name = node.metadata.name
+        # if(node_name==nodeName):
         print(node.metadata.name)
         memory_capacity = node.status.capacity["memory"]
         memory_allocatable = node.status.allocatable["memory"]
     return memory_allocatable, memory_capacity
 
+
 def get_pv_usage(v1, pvName):
     pv_list = v1.list_persistent_volume()
     storage_capacity = None
     storage_usage = None
-    
+
     for pv in pv_list.items:
         pv_name = pv.metadata.name
 
-        if(pv_name == pvName):
+        if (pv_name == pvName):
             storage_capacity = pv.spec.capacity["storage"]
-            #storage_usage = pv.status.capacity["storage"]
-    
+            # storage_usage = pv.status.capacity["storage"]
+
     return storage_usage, storage_capacity
+
 
 @app.route('/', methods=['POST'])
 def handle_request():
     config.load_incluster_config()
 
     v1 = client.CoreV1Api()
-    
-    memory_allocatable, memory_capacity = get_mem_usage(v1,"")
+
+    memory_allocatable, memory_capacity = get_mem_usage(v1, "")
     storage_usage, storage_capacity = get_pv_usage(v1, "shared-volume")
     directory_path = '/shared'
 
@@ -63,18 +66,18 @@ def handle_request():
     volume_usage = get_host_volume_usage(directory_path)
 
     # Print the results
-    print(storage_capacity,storage_usage)
-    print(memory_capacity,memory_allocatable)
+    print(storage_capacity, storage_usage)
+    print(memory_capacity, memory_allocatable)
     print(f"Total Size: {volume_usage['total_size']:.5f} MB")
     print(f"Used Size: {volume_usage['used_size']:.5f} MB")
     print(f"Free Size: {volume_usage['free_size']:.5f} MB")
 
     mem_cap = int(memory_capacity[0:-2])
     mem_aloc = int(memory_allocatable[0:-2])
-    if(mem_aloc/mem_cap>0.2 and volume_usage['free_size'] > 50):
+    if (mem_aloc/mem_cap > 0.2 and volume_usage['free_size'] > 50):
         return 'True'
     else:
-        return 'False' 
+        return 'False'
 
 
 if __name__ == '__main__':
