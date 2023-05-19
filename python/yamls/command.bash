823d40c890668e99e97ec70c4f80993add1a6530
ryan910707 <74304456+ryan910707@users.noreply.github.com>
2023-05-19 22:42:17 +0800

    Update command.bash

--- python/yamls/command.bash
+++ python/yamls/command.bash
@@ -2,11 +2,10 @@
 
 # sample value for your variables
 MANAGER_URL=$(kubectl get pod image-scale --template '{{.status.podIP}}')
-LOCAL_STORAGE = $(curl -X POST $MANAGER_URL:9090 -H 'Content-Type: application/json' -d '{"Query":"local_storage"}')
 
 # read the yml template from a file and substitute the string 
 # {{MYVARNAME}} with the value of the MYVARVALUE variable
-template=`cat "steps.yaml.template" | sed "s/tmp_manager_url/$MANAGER_URL/g" | sed "s/tmp_local_storage/$LOCAL_STORAGE/g"`
+template=`cat "steps.yaml.template" | sed "s/tmp_manager_url/$MANAGER_URL/g"`
 
 # apply the yml with the substituted value
 echo "$template" | kubectl apply -f -
