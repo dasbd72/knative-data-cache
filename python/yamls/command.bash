226122048cad9fedb4e79064f82a700396b86cdc
ryan910707 <74304456+ryan910707@users.noreply.github.com>
2023-05-20 14:07:42 +0800

    Update command.bash

--- python/yamls/command.bash
+++ python/yamls/command.bash
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # sample value for your variables
-MANAGER_URL=$(kubectl get pod image-scale --template '{{.status.podIP}}')
+MANAGER_URL=$(kubectl get ksvc manager --template '{{.status.url}}')
 
 # read the yml template from a file and substitute the string 
-# {{MYVARNAME}} with the value of the MYVARVALUE variable
 template=`cat "steps.yaml.template" | sed "s/tmp_manager_url/$MANAGER_URL/g"`
 
 # apply the yml with the substituted value
