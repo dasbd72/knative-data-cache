facefda63ea56bb33090c769d21b80de2da45eed
dasbd72 <twbd723@gmail.com>
2023-06-22 17:21:43 +0800

    changed MANAGER_URL

--- python/start_service.sh
+++ python/start_service.sh
@@ -6,9 +6,10 @@ kubectl apply -f yamls/pvc.yaml
 kubectl apply -f yamls/clusterrole.yaml
 kubectl apply -f yamls/clusterrolebinding.yaml
 kubectl apply -f yamls/manager.yaml
+kubectl apply -f yamls/manager.yaml
 
 # sample value for your variables
-MANAGER_URL="http://manager:8080"
+MANAGER_URL=$(kubectl get service manager --template '{{.status.url}}')
 
 # read the yml template from a file and substitute the string 
 # {{MYVARNAME}} with the value of the MYVARVALUE variable
