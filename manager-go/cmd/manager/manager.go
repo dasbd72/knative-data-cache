0a199c166ce6196db86c5391ff599a49f89b6084
dasbd72 <twbd723@gmail.com>
2023-08-27 19:59:17 +0800

    feat: manager lru beta v2

--- manager-go/cmd/manager/manager.go
+++ manager-go/cmd/manager/manager.go
@@ -6,8 +6,9 @@ import (
 	"log"
 	"net"
 	"os"
+	"syscall"
 
-	"github.com/dasbd72/images-processing-benchmarks/manager-go/pkg/lru"
+	"github.com/dasbd72/images-processing-benchmarks/manager-go/pkg/filelru"
 	"github.com/dasbd72/rfsnotify"
 )
 
@@ -24,7 +25,6 @@ type Response struct {
 var (
 	storagePath string
 	hostIP      string
-	fileLRU     lru.LRU
 )
 
 func init() {
@@ -44,11 +44,11 @@ func init() {
 }
 
 func main() {
-	go handle_connections()
-	handle_file_events()
+	go handleConnections()
+	handleFileEvents()
 }
 
-func handle_connections() {
+func handleConnections() {
 	listener, err := net.Listen("tcp", ":12345")
 	if err != nil {
 		log.Fatal(err)
@@ -63,12 +63,12 @@ func handle_connections() {
 			log.Fatal(err)
 		}
 
-		go handle_connection(conn)
+		go handleConnection(conn)
 	}
 
 }
 
-func handle_connection(conn net.Conn) {
+func handleConnection(conn net.Conn) {
 	reader := bufio.NewReader(conn)
 	for {
 		var req Request
@@ -91,8 +91,8 @@ func handle_connection(conn net.Conn) {
 	}
 }
 
-func handle_file_events() {
-	// create rfsnotify watcher
+func handleFileEvents() {
+	// initialize watcher
 	watcher, err := rfsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
@@ -104,20 +104,93 @@ func handle_file_events() {
 		log.Fatal(err)
 	}
 
+	// initialize file LRU
+	lru := filelru.NewLRU()
+	err = lru.Init(storagePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// handle file events
 	for {
+		log.Println("Waiting for file events...")
 		select {
 		case event := <-watcher.Events:
-			if event.Has(rfsnotify.Write) || event.Has(rfsnotify.Create) || event.Has(rfsnotify.Chmod) {
-				log.Println("modified file:", event.Name)
-				fileLRU.Push(event.Name)
-			} else if event.Has(rfsnotify.Remove) {
-				log.Println("removed file:", event.Name)
-				fileLRU.Remove(event.Name)
-			} else {
-				log.Println("[WARNING] other event: ", event)
+			log.Println("event: ", event)
+			err = handleFileEvent(event, lru)
+			if err != nil {
+				log.Fatal(err)
 			}
+
 		case err := <-watcher.Errors:
-			log.Println("[ERROR] error:", err)
+			log.Println("[ERROR] event error:", err)
 		}
 	}
+}
+
+func handleFileEvent(event rfsnotify.Event, lru *filelru.LRU) error {
+	if event.Has(rfsnotify.Write) || event.Has(rfsnotify.Create) || event.Has(rfsnotify.Chmod) {
+		log.Println("event modified file:", event.Name)
+		err := lru.Push(event.Name)
+		if err != nil {
+			return err
+		}
+	} else if event.Has(rfsnotify.Remove) {
+		log.Println("event removed file:", event.Name)
+		err := lru.Remove(event.Name)
+		if err != nil {
+			return err
+		}
+	} else {
+		log.Println("[WARNING] other event: ", event)
+	}
+
+	// check storage usage
+	var (
+		fs   syscall.Statfs_t
+		all  uint64
+		free uint64
+		used uint64
+	)
+
+	scan := func() error {
+		err := syscall.Statfs(storagePath, &fs)
+		if err != nil {
+			return err
+		}
+
+		all = fs.Blocks * uint64(fs.Bsize)
+		free = fs.Bfree * uint64(fs.Bsize)
+		used = all - free
+
+		return nil
+	}
+
+	// first scan
+	err := scan()
+	if err != nil {
+		return err
+	}
+	log.Printf("Storage usage: %d/%d\n", used, all)
+
+	// remove the least recently used file
+	for !lru.Empty() && float64(used) > 0.8*float64(all) {
+		file, err := lru.Pop()
+		if err != nil {
+			return err
+		}
+		log.Println("Removing file: ", file)
+
+		err = os.Remove(file)
+		if err != nil {
+			return err
+		}
+
+		err = scan()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
