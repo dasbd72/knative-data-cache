84b2c6edff15987ac02f0b97e0dfbe60b02c98e8
dasbd72 <twbd723@gmail.com>
2023-09-19 01:16:30 +0800

    update commands

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 go mod download
 
-# VideoProcessing
-go run cmd/benchmark/* -bucket video-processing -source original-video -tasks 1 -distribution seq -workflow-type VideoProcessing -force-remote
-go run cmd/benchmark/* -bucket video-processing -source original-video -tasks 1 -distribution seq -workflow-type VideoProcessing
+# Sample commands
+# # VideoProcessing
+# go run cmd/benchmark/* -bucket video-processing -source original-video -tasks 1 -distribution seq -workflow-type VideoProcessing -force-remote
+# go run cmd/benchmark/* -bucket video-processing -source original-video -tasks 1 -distribution seq -workflow-type VideoProcessing
+
+# # ImageProcessing
+# go run cmd/benchmark/* -bucket stress-benchmark -source larger_image -tasks 1 -distribution seq -workflow-type ImageProcessing
+# go run cmd/benchmark/* -bucket stress-benchmark -source larger_image -tasks 1 -distribution seq -workflow-type ImageProcessing -use-mem
+# go run cmd/benchmark/* -bucket stress-benchmark -source larger_image -tasks 1 -distribution seq -workflow-type ImageProcessing -force-remote
+
+# Benchmark
+go run cmd/benchmark/* -bucket video-processing -source original-video -tasks 5 -distribution seq -workflow-type VideoProcessing -force-remote
+go run cmd/benchmark/* -bucket video-processing -source original-video -tasks 5 -distribution seq -workflow-type VideoProcessing
